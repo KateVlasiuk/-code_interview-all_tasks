175c2863198b383d27c0a3c0e64f13e74d4a3368
KaveVlasiuk <kate.vlasiuk@gmail.com>
2019-02-24 13:25:29 -0800

    first commit

--- src/main/java/com/kate/code_interview/all_tasks/task_1/Main.java
+++ src/main/java/com/kate/code_interview/all_tasks/task_1/Main.java
@@ -1,4 +1,20 @@
 package com.kate.code_interview.all_tasks.task_1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class Main {
+
+    public static void main(String[] args) {
+     Solution solve = new Solution();
+
+     solve.isUnique("asdfjklassdf");
+     solve.isUniWithout("asdfjklassdf");
+     solve.isUniNlogN("asdfjklassdf");
+    }
+
 }
+
--- src/main/java/com/kate/code_interview/all_tasks/task_1/Solution.java
+++ src/main/java/com/kate/code_interview/all_tasks/task_1/Solution.java
@@ -1,4 +1,34 @@
 package com.kate.code_interview.all_tasks.task_1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+
+//1.1
+
+    public boolean isUnique(String s) {
+        Map<Character, Integer> uniMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (uniMap.getOrDefault(s.charAt(i),0) == 0) uniMap.put(s.charAt(i), 1);
+            else return false;
+        }
+        return true;
+    }
+    public boolean isUniWithout(String s){
+        for( int i = 0; i < s.length(); i++){
+            if (s.indexOf(s.charAt(i)) != s.lastIndexOf(s)) return false;
+        }
+        return true;
+    }
+
+    public boolean isUniNlogN(String s){
+        char [] charArr = new char[s.length()];
+        Arrays.sort(charArr);
+        for (int i = 0; i < charArr.length; i++){
+            if (charArr[i] == charArr[i+1]) return false;
+        }
+        return true;
+    }
 }
